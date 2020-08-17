0fb6ddacbcbacd8484a7511462aec7322c643d10
raulshma <raulshma@gmail.com>
2020-08-17 18:13:58 +0000

    Deploying to gh-pages from  @ 6e4afc7ab7db772b276213cdc8c842bfc0a888ea 🚀

---
+++ precache-manifest.d19babd1570b677de2e22d41c9194ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa411037d52830cfee315ab134ceb4b5",
+    "url": "/resources-list/index.html"
+  },
+  {
+    "revision": "4ae7acecf0cde80b7785",
+    "url": "/resources-list/static/css/main.735e3e38.chunk.css"
+  },
+  {
+    "revision": "beed8a16f53a5048aa6a",
+    "url": "/resources-list/static/js/2.1893273a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/resources-list/static/js/2.1893273a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae7acecf0cde80b7785",
+    "url": "/resources-list/static/js/main.3ef55d71.chunk.js"
+  },
+  {
+    "revision": "0cba3573fe9494b39757",
+    "url": "/resources-list/static/js/runtime-main.ad0d8d1c.js"
+  }
+]);
