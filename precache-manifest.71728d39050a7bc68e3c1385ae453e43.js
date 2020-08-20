b374a9caab2b99b1b4db08f344500b5b637e1c86
raulshma <raulshma@gmail.com>
2020-08-20 20:36:23 +0000

    Deploying to gh-pages from  @ 55f0b92b5bb3f1f37200ab8c5f70ffc2b71f8b33 🚀

---
+++ precache-manifest.71728d39050a7bc68e3c1385ae453e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c86e02c4d553fed930a72aa33a90f4",
+    "url": "/resources-list/index.html"
+  },
+  {
+    "revision": "f7ae6a78ce620d9fbf3d",
+    "url": "/resources-list/static/css/main.5e2d8468.chunk.css"
+  },
+  {
+    "revision": "33b623932d1230722a64",
+    "url": "/resources-list/static/js/2.899e4673.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/resources-list/static/js/2.899e4673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ae6a78ce620d9fbf3d",
+    "url": "/resources-list/static/js/main.6cc699a8.chunk.js"
+  },
+  {
+    "revision": "0cba3573fe9494b39757",
+    "url": "/resources-list/static/js/runtime-main.ad0d8d1c.js"
+  }
+]);
